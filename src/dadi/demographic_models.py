69fca5616bb8dec6c5756e7765801f2fc0033d09
Sarah Turner <turnersarahd@gmail.com>
2024-11-01 15:07:06 -0500

    update demographic models for dadi

--- src/dadi/demographic_models.py
+++ src/dadi/demographic_models.py
@@ -97,7 +97,7 @@ def ital_botr(
     phi = dadi.PhiManip.phi_1D(xx)
 
     # Initial change in pop size from ancestral (wild) pop
-    # This is the population for Brussels sprouts
+    # This is the population for broccoli and cauliflower 
     phi = dadi.Integration.one_pop(phi, xx, T_1, nu=N_domest)
 
     phi = dadi.PhiManip.phi_1D_to_2D(xx, phi)
@@ -106,6 +106,62 @@ def ital_botr(
     phi = dadi.Integration.two_pops(phi, xx, T_2, nu1=N_ital, nu2=N_botr)
 
     sfs = dadi.Spectrum.from_phi(phi, ns, (xx, xx))
+
+    return sfs
+
+@dadi.Numerics.make_extrap_log_func
+def gon_ital_sab(
+    params: Tuple[float, ...], ns: Tuple[int, ...], pts: List[int]
+) -> dadi.Spectrum:
+    """
+    A simple, divergence-only model for kohlrabi, broccoli, and curly kale.
+
+       dom   kale  kohl  broc
+        ?     |     |     |
+    T3  ?     |     |     |
+    __  |-----|-----|     |
+        |                 |
+    T2  | N_domest        |
+    __  |-----------------|
+    T1  |
+    __  | N_domest
+
+    Parameters
+    ----------
+    params: Tuple[float, ...]
+        Demographic model parameters.
+    ns: Tuple[int, ...]
+        Sample sizes for each of the subpopulations. Can be obtained from the
+        observed sfs using the ``sample_sizes`` method.
+    pts: List[int]
+        A list of grid sizes for numerically integrating the distribution of
+        allele frequencies.
+
+    Returns
+    -------
+    A ``dadi.Spectrum`` object.
+    """
+    N_domest, N_kale, N_kohl, N_broc, T_1, T_2, T_3 = params
+
+    xx = dadi.Numerics.default_grid(pts)
+    phi = dadi.PhiManip.phi_1D(xx)
+
+    # Initial change in pop size from ancestral (wild) pop
+    # This is the population for broccoli 
+    phi = dadi.Integration.one_pop(phi, xx, T_1, nu=N_domest)
+
+    # Split off pop2 (kale) after time T_1
+    phi = dadi.PhiManip.phi_1D_to_2D(xx, phi)
+
+    # Integrate population for time T_2
+    phi = dadi.Integration.two_pops(phi, xx, T_2, nu1=N_domest, nu2=N_broc)
+
+    # Split pop3 (kohlrabi) off of cabbage
+    phi = dadi.PhiManip.phi_2D_to_3D_split_1(xx, phi)
+
+    phi = dadi.Integration.three_pops(phi, xx, T_3, nu1=N_kale, nu2=N_broc, nu3=N_kohl)
+
+    sfs = dadi.Spectrum.from_phi(phi, ns, (xx, xx, xx))
 
     return sfs
 
@@ -164,5 +220,6 @@ def wild_cultivars(
 models: Dict[str, Callable] = {
     "cap_gem_vir": cap_gem_vir,
     "ital_botr": ital_botr,
-    "wild_cultivar": wild_cultivars,
+    "gon_ital_sab": gon_ital_sab,
+    # "wild_cultivar": wild_cultivars
 }
